d7addebba7dec9bfa195e47458d60c89bf987d7d
Sakshi Dhavalshankh <sakshid7@Sakshis-MacBook-Pro.local>
2024-07-25 18:22:20 +0530

    Updates

---
+++ precache-manifest.85f9e1cf43d9e170c26358581786621f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e4eb36c4998f31b1c21c7eedcf5853",
+    "url": "/sd1810/sakshi.github.io/index.html"
+  },
+  {
+    "revision": "b9b61deaa90b81c28552",
+    "url": "/sd1810/sakshi.github.io/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "c3704317d4b32efc63bd",
+    "url": "/sd1810/sakshi.github.io/static/css/main.e92d6613.chunk.css"
+  },
+  {
+    "revision": "b9b61deaa90b81c28552",
+    "url": "/sd1810/sakshi.github.io/static/js/2.8e48d46e.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/sd1810/sakshi.github.io/static/js/2.8e48d46e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3704317d4b32efc63bd",
+    "url": "/sd1810/sakshi.github.io/static/js/main.b403be06.chunk.js"
+  },
+  {
+    "revision": "915dfeb5be5be7857489",
+    "url": "/sd1810/sakshi.github.io/static/js/runtime-main.d1e5c44f.js"
+  }
+]);
